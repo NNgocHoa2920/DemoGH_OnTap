d15edb05809d5d39a356572cfe8f6e48a50bf168
Hoa Nguyễn Ngọc <nguyenngochoa292000@gmail.com>
2024-07-23 15:16:54 +0700

    he he

--- DemoGH_OnTap/Controllers/AccountController.cs
+++ DemoGH_OnTap/Controllers/AccountController.cs
@@ -10,11 +10,7 @@ namespace DemoGH_OnTap.Controllers
         {
             _db = db;
         }
-        public IActionResult Index()
-        {
-            return View();
-        }
-
+ 
         public IActionResult DangKy() // tạo ra view đăng kí thui
         {
             return View();
@@ -66,6 +62,46 @@ namespace DemoGH_OnTap.Controllers
                 return RedirectToAction("Index", "Home");
             }
 
+        }
+
+        //hiển thị tất cả danh sách account
+        public IActionResult Index(string name)
+        {
+            //lấy giá trị session có tên account
+            var sessionData = HttpContext.Session.GetString("Account");
+            if(sessionData == null)
+            {
+                ViewData["message"] = "Bạn chwaa đăng nhập hoặc phiên đăng nhập hết hạn";
+            }
+            else
+            {
+                ViewData["message"] = $"Chào mừng {sessionData} đến với bình ngyên vô tận";
+            }
+            //lấy toàn bộ account
+            var accountData = _db.Accounts.ToList();
+
+            //làm phần tìm kiếm
+            //nếu name tìm kiếm rỗng thì nó sẽ trả về toàn bộ dữ liệu
+            if(string.IsNullOrEmpty(name))
+            {
+                return View(accountData);
+            }
+            else
+            {
+                var searchData = _db.Accounts.Where(x => x.Name.ToLower().Contains(name.ToLower())).ToList();
+                //lưu số lượng kết quả tìm thấy vào viewdara và viewbag
+                ViewData["count"] = searchData.Count;
+                ViewBag.Count = searchData.Count;
+                //check tìm kiesm nếu k có
+                if (searchData.Count == 0)
+                {
+                    return View(accountData);
+                }
+                else
+                    return View(searchData);
+            }
+
+           
         }
     }
 }
